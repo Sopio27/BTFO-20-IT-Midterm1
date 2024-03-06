41f05c4408f95a3dc32285eb7926f712db6870c5
Sopio27 <sopioandriadze@gmail.com>
2024-03-06 22:13:01 +0400

    Midterm1

--- tst.py
+++
@@ -1,9 +0,0 @@
-def check_integer(input_value):
-    return input_value.isdigit()
-
-# Example usage
-value_to_check = input("Enter a value: ")
-if check_integer(value_to_check):
-    print("Input is an integer.")
-else:
-    print("Input is not an integer.")
